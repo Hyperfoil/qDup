0aa9b9f03e2c5e48017a5ba583bd9fa2d3f2eba9
Will Reichert <willr3@gmail.com>
2017-11-28 21:36:21 -0600

    add LineEmittingStream tests

--- src/test/java/perf/ssh/stream/LineEmittingStreamTest.java
+++ src/test/java/perf/ssh/stream/LineEmittingStreamTest.java
@@ -1,4 +1,27 @@
 package perf.ssh.stream;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class LineEmittingStreamTest {
+
+    @Test
+    public void testBufferShrinks(){
+        LineEmittingStream stream = new LineEmittingStream();
+
+        try {
+            stream.write("foo".getBytes());
+            stream.write("foo".getBytes());
+            stream.write("foo".getBytes());
+            System.out.println(stream.getBufferLength()+" "+ stream.getIndex());
+            stream.write("\n".getBytes());
+            System.out.println(stream.getBufferLength()+" "+ stream.getIndex());
+            stream.write("foo".getBytes());
+            System.out.println(stream.getBufferLength()+" "+ stream.getIndex());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
