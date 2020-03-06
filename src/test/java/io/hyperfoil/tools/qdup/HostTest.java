b47af670ec57ee09bb40e4a5abafa8b61424917a
Will Reichert <willr3@gmail.com>
2020-03-06 09:38:27 -0600

    add HostTest

--- src/test/java/io/hyperfoil/tools/qdup/HostTest.java
+++ src/test/java/io/hyperfoil/tools/qdup/HostTest.java
@@ -1,4 +1,19 @@
 package io.hyperfoil.tools.qdup;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HostTest {
+
+
+   @Test
+   public void username_with_password(){
+      Host h = Host.parse("username:pwd@hostname.with.domain");
+      assertEquals("username",h.getUserName());
+      assertEquals("pwd",h.getPassword());
+      assertEquals("hostname.with.domain",h.getHostName());
+      assertEquals("hostname",h.getShortHostName());
+      assertEquals(Host.DEFAULT_PORT,h.getPort());
+   }
 }
