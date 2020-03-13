c9964540a3ee2bd583322264ddb4315ba3385741
Will Reichert <willr3@gmail.com>
2020-03-12 22:29:32 -0500

    use graaljs for js cmd

--- src/main/java/io/hyperfoil/tools/qdup/cmd/impl/JsCmd.java
+++ src/main/java/io/hyperfoil/tools/qdup/cmd/impl/JsCmd.java
@@ -1,57 +1,30 @@
 package io.hyperfoil.tools.qdup.cmd.impl;
 
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
-import org.slf4j.ext.XLogger;
-import org.slf4j.ext.XLoggerFactory;
-import io.hyperfoil.tools.qdup.State;
 import io.hyperfoil.tools.qdup.cmd.Cmd;
 import io.hyperfoil.tools.qdup.cmd.Context;
+import io.hyperfoil.tools.yaup.StringUtil;
+import org.graalvm.polyglot.Value;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.lang.invoke.MethodHandles;
-import java.util.function.BiFunction;
 
 /**
- * Use nashorn to support BiFunctions in javascript:
+ * Use graaljs to support BiFunctions in javascript:
  * function(input,state){
  *     return true | false;
  * }
- * Returning true means invoke the next command, returning false means skip the next command. No return message assumes a return of true
+ * Returning true means invoke the next command,
+ * Returning false means skip the next command.
+ * No return message assumes a return of true
  */
 public class JsCmd extends Cmd {
     final static XLogger logger = XLoggerFactory.getXLogger(MethodHandles.lookup().lookupClass());
 
-
-    private ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-    public static Object eval(String input){
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-
-        Object rtrn = null;
-        try {
-            rtrn = engine.eval(input);
-        } catch (ScriptException e) {
-            logger.warn(JsCmd.class.getSimpleName()+": "+input+" threw "+e.getMessage(),e);
-        }
-        return rtrn;
-    }
-
     private String codeString;
-    private BiFunction<String,State,Object> function;
 
     public JsCmd(String code){
         this.codeString = code;
-        //trap for ES6 when not running on an ES6 compatible jvm?
-        //NOPE, too many cases to support
-        try {
-            engine.eval("var console = { log: print };");
-            this.function = (BiFunction<String,State,Object>) engine.eval(
-                    String.format("new java.util.function.BiFunction(%s)", codeString));
-        } catch (ScriptException e) {
-            e.printStackTrace();
-        }
-
     }
 
     public String getCode(){
@@ -60,16 +33,14 @@ public class JsCmd extends Cmd {
 
     @Override
     public void run(String input, Context context) {
-        if(function!=null){
             try{
-                Object rtrn = this.function.apply(input,context.getState());
+                Object rtrn = StringUtil.jsEval(codeString,input,context.getState());
                 if( rtrn==null ||
-
                     (rtrn instanceof Boolean && !((Boolean)rtrn)) ||
                     (rtrn instanceof String && ((String)rtrn).toUpperCase().equals("FALSE"))
                 ){
                     context.skip(input);
-                }else if (ScriptObjectMirror.isUndefined(rtrn) || rtrn instanceof Boolean){
+                }else if (rtrn == null || (rtrn instanceof Value && ((Value)rtrn).isNull()) || rtrn instanceof Boolean){
                     //TODO potentially log that the js function should have an explicit return message
                     context.next(input);
                 }else {
@@ -79,10 +50,6 @@ public class JsCmd extends Cmd {
                 //TODO log the failure
                 context.skip(input);
             }
-        }else{
-            //TODO log that the function could not be run becuase it failed to create
-            context.skip(input);
-        }
     }
 
     @Override
