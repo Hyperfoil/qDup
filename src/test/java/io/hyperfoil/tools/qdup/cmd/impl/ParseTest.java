a86f0f62190a7469d6046cee6d866f292abf6079
Will Reichert <willr3@gmail.com>
2021-06-25 14:04:04 -0400

    update hyperfoil dependencies

---
+++ src/test/java/io/hyperfoil/tools/qdup/cmd/impl/ParseTest.java
@@ -0,0 +1,12 @@
+package io.hyperfoil.tools.qdup.cmd.impl;
+
+import io.hyperfoil.tools.qdup.SshTestBase;
+import org.junit.Test;
+
+public class ParseTest extends SshTestBase {
+
+    @Test
+    public void fromYaml(){
+
+    }
+}
