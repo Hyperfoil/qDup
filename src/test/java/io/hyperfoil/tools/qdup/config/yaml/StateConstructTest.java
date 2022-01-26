efeea830245d55b3e6b66f9ced7588d9cf3dea0c
Will Reichert <willr3@gmail.com>
2022-01-26 10:23:50 -0500

    add test for proper yaml json parsing

--- src/test/java/io/hyperfoil/tools/qdup/config/yaml/StateConstructTest.java
+++ src/test/java/io/hyperfoil/tools/qdup/config/yaml/StateConstructTest.java
@@ -2,6 +2,7 @@ package io.hyperfoil.tools.qdup.config.yaml;
 
 import io.hyperfoil.tools.qdup.State;
 
+import io.hyperfoil.tools.yaup.json.Json;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -11,9 +12,7 @@ import org.yaml.snakeyaml.nodes.Tag;
 import io.hyperfoil.tools.yaup.yaml.OverloadConstructor;
 
 import static io.hyperfoil.tools.qdup.State.RUN_PREFIX;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class StateConstructTest {
 
@@ -35,6 +34,20 @@ public class StateConstructTest {
         assertEquals("state[key]=value","value",loaded.get("key"));
         assertEquals("expect 0 children",0,loaded.getChildNames().size());
     }
+
+    @Test
+    public void key_value_object(){
+        State loaded = yaml.loadAs("key: {}",State.class);
+        assertNotNull("should load states",loaded);
+        assertEquals("run prefix",RUN_PREFIX,loaded.getPrefix());
+        Object key = loaded.get("key");
+        assertNotNull("state[key] should exist",key);
+        assertTrue("state[key] should be json",key instanceof Json);
+        Json json = (Json)key;
+        assertFalse("state[key] should be an object "+json,json.isArray());
+        assertEquals("expect 0 children",0,loaded.getChildNames().size());
+    }
+
 
 
 }
